b550453b44add13394dd26ed65b457dea0dc62f5
harrysouza <harrysouza@gmail.com>
2015-04-06 22:42:20 -0300

    Post text field Test

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,6 +3,7 @@
 angular.module('deepDiveApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.questions = [];
+    $scope.answers = [];
 
     $http.get('/api/questions').success(function(questions) {
       $scope.questions = questions;
@@ -20,9 +21,28 @@ angular.module('deepDiveApp')
       $http.delete('/api/questions/' + question._id);
     };
     
-    $scope.generateDash = function() {
-        $http.post('/api/answers', {questions: $scope.questions});
-      };
+    $scope.generateDash = function() { //Não funciona...
+        $http
+        .post('/api/answers', {options2: $scope.answer})
+        .success(function(data){
+            console.log('AAAAAA...SUCESSO');
+        })
+        .error(function(data){
+            console.log('Error: ' + data);
+        });
+    };
+
+    $scope.submit = function() { //Funciona
+    $http
+        .post('/api/answers', {teste: $scope.answer.teste})
+        .success(function(data){
+            console.log('AAAAAA...SUCESSO');
+        })
+        .error(function(data){
+            console.log('Error: ' + data);
+        });
+  };
+
   });
 
     
--- server/api/answer/answer.model.js
+++ server/api/answer/answer.model.js
@@ -4,7 +4,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var AnswerSchema = new Schema({
-  questions: []
+  teste: String,
+  options2: [{ 
+  	opt1: String, 
+  	opt2: String, 
+  	opt3: String, 
+  	opt4: String, 
+  	opt5: String 
+  }]
 });
 
 module.exports = mongoose.model('Answer', AnswerSchema);
